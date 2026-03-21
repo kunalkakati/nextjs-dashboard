5466b57ca47a19356ba634faed317f8a42296ef8
Kunal kakati <kunalkakati877@gmail.com>
2026-03-21 10:34:22 +0530

    Form handling and validating

---
+++ app/lib/action.ts
@@ -0,0 +1,93 @@
+'use server';
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+import z from "zod";
+
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+//validation schema for formData.
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string()
+});
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+// omit/exclude iḍ and date as they will be auto generated.
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    // validation of formData.
+
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    } catch (e) {
+        console.log(e);
+
+    }
+    // redender so that updatation shows in the main page.
+    revalidatePath('/dashboard/invoices');
+    // redirect to main page.
+    redirect('/dashboard/invoices');
+}
+
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData): Promise<void> {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+    const amountInCent = amount * 100;
+
+    try {
+        await sql`UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCent}, status = ${status}
+        WHERE id = ${id}`;
+    } catch (e) {
+        console.log(e);
+
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice');
+    await sql`DELETE from invoices WHERE id=${id}`;
+    revalidatePath('/dashboard/invoices');
+
+}
