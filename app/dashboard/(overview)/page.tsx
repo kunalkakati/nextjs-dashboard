828476562232e294109f738d9644d3e6dd4caf7b
Kunal kakati <kunalkakati877@gmail.com>
2026-03-23 17:37:21 +0530

    complete

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,8 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { CardSkeleton, InvoiceSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
 
-
+export const metadata: Metadata = {
+    title: "Home"
+}
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
@@ -19,14 +22,6 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                <Card title="Pending" value={totalPendingInvoices} type="pending" />
-                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-                <Card
-                    title="Total Customers"
-                    value={numberOfCustomers}
-                    type="customers"
-                /> */}
                 <Suspense fallback={<CardSkeleton />}>
                     <CardWrapper />
                 </Suspense>
@@ -34,7 +29,6 @@ export default async function Page() {
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
-                    {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
                 </Suspense>
                 <Suspense fallback={<InvoiceSkeleton />}>
                     <LatestInvoices />
